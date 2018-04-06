67e2e897d8213cdc48ac41331cee7ca7be13db56
Bo <pangb86@gmail.com>
2018-04-05 17:23:54 -0700

    Add stoplight

---
+++ stoplight/stoplight.js
@@ -0,0 +1,19 @@
+let red = document.querySelector(".red");
+let yellow = document.querySelector(".yellow");
+let green = document.querySelector(".green");
+
+function cycleLight() {
+  if (red.style.opacity == 1.0) {
+    red.style.opacity = 0.3;
+    yellow.style.opacity = 1.0;
+  } else if (yellow.style.opacity == 1.0) {
+    yellow.style.opacity = 0.3;
+    green.style.opacity = 1.0;
+  } else if (green.style.opacity == 1.0) {
+    green.style.opacity = 0.3;
+    red.style.opacity = 1.0;
+  }
+}
+
+red.style.opacity = 1.0;
+setInterval(cycleLight, 1000);
