822f4bb6af44d393eb08f8cdd3b0570c7d0dbdef
Bo <pangb86@gmail.com>
2018-04-07 21:01:58 -0700

    Modify stoplight

--- stoplight/stoplight.js
+++ stoplight/stoplight.js
@@ -1,21 +1,41 @@
 let red = document.querySelector(".red");
 let yellow = document.querySelector(".yellow");
 let green = document.querySelector(".green");
+let lightOn = false;
+let lightButton = document.querySelector("button");
 
-function cycleLight() {
-  if (red.style.opacity == 1.0) {
-    red.style.opacity = 0.3;
-    yellow.style.opacity = 1.0;
-  } else if (yellow.style.opacity == 1.0) {
-    yellow.style.opacity = 0.3;
-    green.style.opacity = 1.0;
-  } else if (green.style.opacity == 1.0) {
-    green.style.opacity = 0.3;
-    red.style.opacity = 1.0;
+function redOn() {
+  red.style.opacity = 1.0;
+  yellow.style.opacity = 0.3;
+  green.style.opacity = 0.3;
+}
+
+function yellowOn() {
+  red.style.opacity = 0.3;
+  yellow.style.opacity = 1.0;
+  green.style.opacity = 0.3;
+}
+
+function greenOn() {
+  red.style.opacity = 0.3;
+  yellow.style.opacity = 0.3;
+  green.style.opacity = 1.0;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function runLight() {
+  lightOn = !lightOn;
+  while (lightOn) {
+    redOn();
+    await sleep(2000);
+    yellowOn();
+    await sleep(1000);
+    greenOn();
+    await sleep(2000);
   }
 }
 
-red.style.opacity = 1.0;
-setInterval(cycleLight, 1000);
-setInterval(cycleLight, 1000);
-setInterval(cycleLight, 1000);
+lightButton.addEventListener("click", runLight);
