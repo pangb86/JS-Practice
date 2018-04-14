8aa2dc256f3bdd1186405f462b8eef8882bedaef
Bo <pangb86@gmail.com>
2018-04-13 18:12:26 -0700

    Modify dropdown

--- dropdown/menu.js
+++ dropdown/menu.js
@@ -1,10 +1,12 @@
 let dropButton = document.querySelector(".menu-button");
 let dropMenu = document.querySelector(".menu-content");
 
+// event listener on button for toggling the dropdown
 dropButton.addEventListener("click", () => {
   dropMenu.classList.toggle("show");
 });
 
+// closes the dropdown if anywhere outside the button is clicked
 document.addEventListener("click", (e) => {
   if (!e.target.matches(".menu-button")) {
     if (dropMenu.classList.contains("show")) {
